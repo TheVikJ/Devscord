94f0fa3d27daee11f758c571cd86c4c576c4086e
Vikram <33194767+Vik0105@users.noreply.github.com>
2020-08-30 14:08:08 +0530

    Make code MUCH more concise

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 import requests
 
-TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 client = discord.Client()
 
@@ -11,185 +11,11 @@ async def on_message(message):
         return
 
     if message.content.startswith('!api-category'):
-        if message.content.startswith('!api-category animals'):
-            await message.channel.send(requests.get('https://api.publicapis.org/entries?category=animals&https=true').text)
+        search = message.content
+        search = search.replace('!api-category ', '')
+        searchterms = ''.join([i for i in search if not i.isdigit()])
 
-        elif message.content.startswith('!api-category anime'):
-            await message.channel.send(requests.get('https://api.publicapis.org/entries?category=anime&https=true').text)
-
-        elif message.content.startswith('!api-category anti-malware'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=anti-malware&https=true').text)
-
-        elif message.content.startswith('!api-category art&design'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=art&https=true').text)
-
-        elif message.content.startswith('!api-category books'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=books&https=true').text)
-
-        elif message.content.startswith('!api-category business'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=business&https=true').text)
-
-        elif message.content.startswith('!api-category calendar'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=calendar&https=true').text)
-
-        elif message.content.startswith('!api-category cloudstorage&filesharing'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=cloud&https=true').text)
-
-        elif message.content.startswith('!api-category continuousintegration'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=continuous&https=true').text)
-
-        elif message.content.startswith('!api-category cryptocurrency'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=cryptocurrency&https=true').text)
-
-        elif message.content.startswith('!api-category currencyexchange'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=currency&https=true').text)
-
-        elif message.content.startswith('!api-category datavalidation'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=data&https=true').text)
-
-        elif message.content.startswith('!api-category development'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=development&https=true').text)
-
-        elif message.content.startswith('!api-category dictionaries'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=dictionaries&https=true').text)
-
-        elif message.content.startswith('!api-category documents&productivity'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=documents&https=true').text)
-
-        elif message.content.startswith('!api-category environment'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=environment&https=true').text)
-
-        elif message.content.startswith('!api-category events'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=events&https=true').text)
-
-        elif message.content.startswith('!api-category finance'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=finance&https=true').text)
-
-        elif message.content.startswith('!api-category food&drink'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=food&https=true').text)
-
-        elif message.content.startswith('!api-category games&comics'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=games&https=true').text)
-
-        elif message.content.startswith('!api-category geocoding'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=geocoding&https=true').text)
-
-        elif message.content.startswith('!api-category government'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=government&https=true').text)
-
-        elif message.content.startswith('!api-category health'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=health&https=true').text)
-
-        elif message.content.startswith('!api-category jobs'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=jobs&https=true').text)
-
-        elif message.content.startswith('!api-category machinelearning'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=machine&https=true').text)
-
-        elif message.content.startswith('!api-category music'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=music&https=true').text)
-
-        elif message.content.startswith('!api-category news'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=news&https=true').text)
-
-        elif message.content.startswith('!api-category opendata'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=open&https=true').text)
-
-        elif message.content.startswith('!api-category opensourceprojects'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=projects&https=true').text)
-
-        elif message.content.startswith('!api-category patent'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=patent&https=true').text)
-
-        elif message.content.startswith('!api-category personality'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=personality&https=true').text)
-
-        elif message.content.startswith('!api-category photography'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=photography&https=true').text)
-
-        elif message.content.startswith('!api-category science&math'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=science&https=true').text)
-
-        elif message.content.startswith('!api-category security'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=security&https=true').text)
-
-        elif message.content.startswith('!api-category shopping'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=shopping&https=true').text)
-
-        elif message.content.startswith('!api-category social'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=social&https=true').text)
-
-        elif message.content.startswith('!api-category sports&fitness'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=sports&https=true').text)
-
-        elif message.content.startswith('!api-category testdata'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=test&https=true').text)
-
-        elif message.content.startswith('!api-category textanalysis'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=text&https=true').text)
-
-        elif message.content.startswith('!api-category tracking'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=tracking&https=true').text)
-
-        elif message.content.startswith('!api-category transportation'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=transportation&https=true').text)
-
-        elif message.content.startswith('!api-category urlshorteners'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=url&https=true').text)
-
-        elif message.content.startswith('!api-category vehicle'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=vehicle&https=true').text)
-
-        elif message.content.startswith('!api-category video'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=video&https=true').text)
-
-        elif message.content.startswith('!api-category weather'):
-            await message.channel.send(
-                requests.get('https://api.publicapis.org/entries?category=weather&https=true').text)
-
-        else:
+        if searchterms == '':
             await message.channel.send(
                 'Please use ```!api-category [name]```to get APIs of that category. The current categories'
                 'are Animals, Anime, Anti-Malware, Art&Design, Books, Business, Calendar, Cloud Storage'
@@ -199,23 +25,33 @@ async def on_message(message):
                 'MachineLearning, Music, News, OpenData, OpenSourceProjects, Patent, Personality, '
                 'Photography, Science&Math, Security, Shopping, Social, Sports&Fitness, TestData'
                 'TextAnalysis, Tracking, Transportation, URLShorteners, Vehicle, Video, Weather.')
+        else:
+            url = 'https://api.publicapis.org/entries?category=' + searchterms + '&https=true'
+            r = requests.get(url).text
+            msg = ''
+            if len(r)>2000:
+                for i in range(0, 1999):
+                    msg += r[i]
+            else:
+                msg = r
+            await message.channel.send(msg)
 
     if message.content.startswith('!api-random'):
         await message.channel.send(requests.get('https://api.publicapis.org/random?auth=null').text)
-        
+
     if message.content.startswith('!api-search'):
         desc = message.content
         minus = '!api-search '
         desc = desc.replace(minus, '')
         url = 'https://api.publicapis.org/entries?description=' + desc + '&https=true'
         await message.channel.send(requests.get(url).text)
-        
+
     if message.content.startswith('!stack-search'):
         searchterms = message.content
         searchterms = searchterms.replace('!stack-search ', '')
-        msg = requests.get('https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=' + searchterms + '&site=stackoverflow').text
+        msg = requests.get('https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=' + searchterms + '&site=stackoverflow')
         await message.channel.send(msg)
-           
+
 @client.event
 async def on_ready():
     print('Logged in as')
